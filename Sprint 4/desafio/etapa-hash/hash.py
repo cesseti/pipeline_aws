3312699765e30be6655232a518dbe3b8dcbc330e
cesseti <carlosribeiro.cr146@gmail.com>
2024-12-20 17:50:51 -0300

    Terminando de ajustar read.me do desafio

--- Sprint 4/desafio/etapa-hash/hash.py
+++ Sprint 4/desafio/etapa-hash/hash.py
@@ -1,10 +1,10 @@
 import hashlib
 
 # Input para receber string
-mensagem = input('Escreva uma mensagem: ')
+string = input('Digite uma palavra para descobrir seu hash: ')
 
 # Gera o hash SHA-1
-hash_sha1 = hashlib.sha1(mensagem.encode())
+hash_sha1 = hashlib.sha1(string.encode())
 
 # Exibe o resultado
-print(f"Esse é a sua mensagem: {mensagem}\nE esse é a sua mensagem em hash: {hash_sha1.hexdigest()}")
+print(f"Esse é o hash da sua palavra: {hash_sha1.hexdigest()}")
